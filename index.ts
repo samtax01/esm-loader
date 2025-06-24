cae1785e31ae9e590e039e568971032463441019
samtax01 <samsoniyanu@hotmail.com>
2025-06-24 17:33:50 -0400

    feat: esm-loader demos

---
+++ index.ts
@@ -0,0 +1,6 @@
+import {a} from './ts-module.ts'
+import {b} from './js-module.js'
+
+const sum: number = a + b;
+
+console.log("sum: ", sum)
---
+++ loader.js
@@ -0,0 +1,11 @@
+import { transform } from 'esbuild' 
+
+export async function load (url, context, nextLoad) {
+    if (url.endsWith('.ts')) {
+        const { source, format } = await nextLoad(url, {...context, format: "module"})
+        const { code } = await transform(source, {loader: 'ts'})
+        return {source: code, format}
+    }
+
+    return await nextLoad(url, context)
+}
